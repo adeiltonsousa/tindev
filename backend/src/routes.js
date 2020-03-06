c38bc719a904745e346f13776e2079f44c069a20
Adeilton Sousa <adeiltonsousaxavier@gmail.com>
2020-03-06 08:54:04 -0300

    Commit inicial com configuraçoes do back e Express

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  return res.json({message: `Hello ${req.query.name}` });
+  return res.json({message: `Olá ${req.query.name}` });
 });
+
+routes.post('/devs', (req, res) => {
+  return res.json(req.body);
+})
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://adeilton:iltinho25@tindev-kuneg.mongodb.net/tindevdb?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
