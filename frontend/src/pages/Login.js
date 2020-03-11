703d415de949c7a874178eee3506db4aa2897142
Adeilton Sousa <adeiltonsousaxavier@gmail.com>
2020-03-11 16:42:59 -0300

    Componente de Login estatico

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <form>
+        <img src={logo} alt="Logo Tindev"/>
+        <input placeholder="Digite seu Usuário do Github"/>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
